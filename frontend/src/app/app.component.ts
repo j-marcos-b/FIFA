3fb3e6c9a0a82004c11c3ec308d0de8eb0726266
j-marcos-b <correodemarcosb@gmail.com>
2025-06-22 00:27:09 -0300

    Se refactoriza el navbar - centra búsqueda y alinea botones a la derecha. Se añade sistema de tarjetas 3D con efecto flip

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ListPlayersComponent } from "./components/list-players/list-players.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ListPlayersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/components/list-players/list-players.component.ts
+++ frontend/src/app/components/list-players/list-players.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../../interfaces/player';
 
 @Component({
   selector: 'app-list-players',
@@ -6,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './list-players.component.html',
   styleUrl: './list-players.component.css'
 })
-export class ListPlayersComponent {
+export class ListPlayersComponent implements OnInit {
+  listPlayers: Player[] = [
+    {
+      id: 1,
+      long_name: 'Lionel Messi',
+      player_face_url: 'https://example.com/messi.jpg',
+      club_name: 'Paris Saint-Germain',
+      player_positions: 'RW',
+      fifa_version: '22',
+      age: 34
+    },
+    {
+      id: 2,
+      long_name: 'Cristiano Ronaldo',
+      player_face_url: 'https://example.com/ronaldo.jpg',
+      club_name: 'Manchester United',
+      player_positions: 'ST',
+      fifa_version: '22',
+      age: 36
+    },
+    {
+      id: 3,
+      long_name: 'Neymar Jr.',
+      player_face_url: 'https://example.com/neymar.jpg',
+      club_name: 'Paris Saint-Germain',
+      player_positions: 'LW',
+      fifa_version: '22',
+      age: 29
+    }
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
