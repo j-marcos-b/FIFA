3f7ef64b30beb13e4e2eb034edf7eb52c6a16cbf
j-marcos-b <correodemarcosb@gmail.com>
2025-06-21 21:11:24 -0300

    Crear interfaz Player con datos clave para identificación y gráficos de evolución

--- frontend/src/app/interfaces/player.ts
+++ frontend/src/app/interfaces/player.ts
@@ -1,2 +1,18 @@
 export interface Player {
+  id: number;
+  long_name: string;
+  player_face_url: string;
+  club_name?: string;
+  nationality_name?: string;
+  player_positions: string;
+  fifa_version: string;
+  age: number;
+
+  // Skills clave para gráfico radar o evolución
+  pace?: number;
+  shooting?: number;
+  passing?: number;
+  dribbling?: number;
+  defending?: number;
+  physic?: number;
 }
