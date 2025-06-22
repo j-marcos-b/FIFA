417d8ad41716f39c4bda6ece684448a3c1e74319
j-marcos-b <correodemarcosb@gmail.com>
2025-06-21 21:05:42 -0300

    Carpetas iniciales: services, interfaces, shared, component

---
+++ frontend/src/app/components/add-edit-players/add-edit-players.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-players',
+  imports: [],
+  templateUrl: './add-edit-players.component.html',
+  styleUrl: './add-edit-players.component.css'
+})
+export class AddEditPlayersComponent {
+
+}
---
+++ frontend/src/app/components/list-players/list-players.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-players',
+  imports: [],
+  templateUrl: './list-players.component.html',
+  styleUrl: './list-players.component.css'
+})
+export class ListPlayersComponent {
+
+}
