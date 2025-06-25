e6a9bc607116aee47711ee240f314b89d3a3a64a
j-marcos-b <correodemarcosb@gmail.com>
2025-06-24 23:21:01 -0300

    se crea el  modelo Sequelize para Player con todos sus campos

---
+++ server/src/models/player-models.ts
@@ -0,0 +1,99 @@
+import { DataTypes } from "sequelize";
+import db from "../db/connection";
+
+const Player = db.define('Player', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    long_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    player_face_url: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    club_name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    nationality_name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    player_positions: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    fifa_version: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    overall: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    height_cm: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    weight_kg: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    preferred_foot: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    pace: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    shooting: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    passing: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    dribbling: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    defending: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    physic: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    attacking_finishing: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    skill_ball_control: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    movement_reactions: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    power_shot_power: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+}, {
+    tableName: 'players',
+    timestamps: false
+});
+
+export default Player;
